be474fad42c8b36e93ec59eab502fa4ad01d6475
Marius Gedminas <marius@gedmin.as>
2014-05-16 11:34:37 +0300

    Replace PNG icons with shields.io-style SVG icons
    Fixes https://issues.jenkins-ci.org/browse/JENKINS-23062 and
    https://issues.jenkins-ci.org/browse/JENKINS-21112

--- src/main/java/org/jenkinsci/plugins/badge/ImageResolver.java
+++ src/main/java/org/jenkinsci/plugins/badge/ImageResolver.java
@@ -33,11 +33,11 @@ public class ImageResolver {
     
     public ImageResolver() throws IOException{
         images = new StatusImage[] {
-                new StatusImage("failure.png"),
-                new StatusImage("unstable.png"),
-                new StatusImage("success.png"),
-                new StatusImage("running.png"),
-                new StatusImage("unknown.png")
+                new StatusImage("build-failure-red.svg"),
+                new StatusImage("build-unstable-yellow.svg"),
+                new StatusImage("build-passing-brightgreen.svg"),
+                new StatusImage("build-running-blue.svg"),
+                new StatusImage("build-unknown-lightgrey.svg")
         };
     }
     
