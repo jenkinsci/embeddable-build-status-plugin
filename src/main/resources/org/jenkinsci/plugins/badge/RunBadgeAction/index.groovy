e712a04b5bac196af582dc3df727c937148d9ef7
Christian Galsterer <christian.galsterer@gmx.de>
2015-02-08 07:50:47 +0100

    [JENKINS-26585] - Two new variants to show also direct link to job without view in URL

--- src/main/resources/org/jenkinsci/plugins/badge/RunBadgeAction/index.groovy
+++ src/main/resources/org/jenkinsci/plugins/badge/RunBadgeAction/index.groovy
@@ -30,44 +30,79 @@ l.layout {
 </style>
 """)
 
-        def base =  "${app.rootUrl}${my.project.url}${my.run.number}/";
-        def badge = base + "badge/icon"
-        
         def fullJobName = h.escape(my.project.fullName);
-        def publicbadge = "${app.rootUrl}buildStatus/icon?job=${fullJobName}&build=${my.run.number}";
+        def jobUrlWithView =  "${app.rootUrl}${my.project.url}${my.run.number}/";
+        def jobUrlWithoutView =  "${app.rootUrl}job/${fullJobName}/${my.run.number}/";
+        def badgeUrlWithView = jobUrlWithView + "badge/icon"
+        def badgeUrlWithoutView = jobUrlWithoutView + "/badge/icon"
+        def publicBadge = "${app.rootUrl}buildStatus/icon?job=${fullJobName}&build=${my.run.number}";
+
         h3 {
             text(_("Image"))
-            img(id:"badge",src:badge)
+            img(id:"badgeUrlWithView",src:badgeUrlWithView)
             text(_(" or "))
-            img(src:badge + "?style=plastic")
+            img(src:badgeUrlWithView + "?style=plastic")
         }
-        b {text(_("protected"))}
-        input(type:"text",value:badge,class:"select-all")
-        b {text(_("unprotected"))}
-        input(type:"text",value:publicbadge,class:"select-all")
 
-        h3(_("Markdown"))
+        h3(_("Plain Link (with view)"))
         b {text(_("protected"))}
-        input(type:"text",value:"[![Build Status](${badge})](${base})",class:"select-all")
+        input(type:"text",value:badgeUrlWithView,class:"select-all")
         b {text(_("unprotected"))}
-        input(type:"text",value:"[![Build Status](${publicbadge})](${base})",class:"select-all")
+        input(type:"text",value:publicBadge,class:"select-all")
 
-        h3(_("HTML"))
+        h3(_("Plain Link (without view)"))
         b {text(_("protected"))}
-        input(type:"text",value:"<a href='${base}'><img src='${badge}'></a>",class:"select-all")
+        input(type:"text",value:badgeUrlWithoutView,class:"select-all")
         b {text(_("unprotected"))}
-        input(type:"text",value:"<a href='${base}'><img src='${publicbadge}'></a>",class:"select-all")
+        input(type:"text",value:publicBadge,class:"select-all")
 
-        h3(_("Confluence"))
-        b {text(_("protected"))}
-        input(type:"text",value:"[!${badge}!|${base}]",class:"select-all")
-        b {text(_("unprotected"))}
-        input(type:"text",value:"[!${publicbadge}!|${base}]",class:"select-all")
 
-        h3(_("XWiki"))
+        h3(_("Markdown (with view)"))
         b {text(_("protected"))}
-        input(type:"text",value:"[[image:${badge}>>${base}||target='__new']]",class:"select-all")
+        input(type:"text",value:"[![Build Status](${badgeUrlWithView})](${jobUrlWithView})",class:"select-all")
         b {text(_("unprotected"))}
-        input(type:"text",value:"[[image:${publicbadge}>>${base}||target='__new']]",class:"select-all")
+        input(type:"text",value:"[![Build Status](${publicBadge})](${jobUrlWithView})",class:"select-all")
+
+        h3(_("Markdown (without view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"[![Build Status](${badgeUrlWithoutView})](${jobUrlWithoutView})",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"[![Build Status](${publicBadge})](${jobUrlWithoutView})",class:"select-all")
+
+        h3(_("HTML (with view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"<a href='${jobUrlWithView}'><img src='${badgeUrlWithView}'></a>",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"<a href='${jobUrlWithView}'><img src='${publicBadge}'></a>",class:"select-all")
+
+        h3(_("HTML (without view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"<a href='${jobUrlWithoutView}'><img src='${badgeUrlWithoutView}'></a>",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"<a href='${jobUrlWithoutView}'><img src='${publicBadge}'></a>",class:"select-all")
+
+        h3(_("Confluence (with view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"[!${badgeUrlWithView}!|${jobUrlWithView}]",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"[!${publicBadge}!|${jobUrlWithView}]",class:"select-all")
+
+        h3(_("Confluence (without view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"[!${badgeUrlWithoutView}!|${jobUrlWithoutView}]",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"[!${publicBadge}!|${jobUrlWithoutView}]",class:"select-all")
+
+        h3(_("XWiki (with view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"[[image:${badgeUrlWithView}>>${jobUrlWithView}||target='__new']]",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"[[image:${publicBadge}>>${jobUrlWithView}||target='__new']]",class:"select-all")
+
+        h3(_("XWiki (without view)"))
+        b {text(_("protected"))}
+        input(type:"text",value:"[[image:${badgeUrlWithoutView}>>${jobUrlWithoutView}||target='__new']]",class:"select-all")
+        b {text(_("unprotected"))}
+        input(type:"text",value:"[[image:${publicBadge}>>${jobUrlWithoutView}||target='__new']]",class:"select-all")
     }
 }
