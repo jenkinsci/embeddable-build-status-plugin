f36f4bc93a28b089f91461c6b7eba509c6874b7a
Kalarani <kalarani@thoughtworks.com>
2022-10-29 12:58:34 +0530

    #114 Add test for Parameter resolver in default package

---
+++ src/test/java/org/jenkinsci/plugins/badge/ParameterResolverTest.java
@@ -0,0 +1,22 @@
+package org.jenkinsci.plugins.badge;
+
+import hudson.model.Actionable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class ParameterResolverTest {
+    @ParameterizedTest
+    @CsvSource({
+            "Build ${params.BUILD_BRANCH},Build params.BUILD_BRANCH",
+            "Build ${params.BUILD_BRANCH|master},Build params.BUILD_BRANCH|master",
+            "Build ${params.BUILD_BRANCH|master} (${displayName}),Build params.BUILD_BRANCH|master (displayName)"
+    })
+    void shouldResolveSubjectWithVariables(String queryParameter, String expectedParameter) {
+        String resolvedParameter = new ParameterResolver().resolve(Mockito.mock(Actionable.class), queryParameter);
+        assertThat(resolvedParameter, is(expectedParameter));
+    }
+}
