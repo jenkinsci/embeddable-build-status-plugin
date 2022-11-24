5449000c2ac11a7e9ac0988f7f7c463841ddbe6c
Okhan-coder <O_khan4@u.pacific.edu>
2022-11-24 02:15:28 -0800

    Trying to make first test work

---
+++ src/test/java/org/jenkinsci/plugins/badge/ImageResolverTest.java
@@ -0,0 +1,29 @@
+package org.jenkinsci.plugins.badge;
+
+import hudson.model.BallColor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ImageResolverTest {
+    ImageResolver ImageTester;
+    
+    @Test
+    void BallShouldBeDefault(){
+        ImageTester = new ImageResolver();
+        String s = "Hello";
+        String style="ball-32x32";
+        String status = "passing";
+        String subject = "build";
+       
+        ImageTester.getImage(BallColor.BLUE,style,status,subject, null, null, null);
+
+    }
+
+
+}
