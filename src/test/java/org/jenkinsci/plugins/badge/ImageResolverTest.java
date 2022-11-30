74df5e5120f6271012e5b88a546c999c0b04eb83
Okhan-coder <O_khan4@u.pacific.edu>
2022-11-30 00:25:24 -0800

    Fixing Mock Objects

--- src/test/java/org/jenkinsci/plugins/badge/ImageResolverTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/ImageResolverTest.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.Mockito;
+import org.mockito.Spy;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class ImageResolverTest {
     ImageResolver ImageTester;
@@ -21,8 +24,18 @@ public class ImageResolverTest {
         String status = "passing";
         String subject = "build";
         String colorName=null;
-        ImageTester.getImage(BallColor.BLUE,style,status,subject, colorName, null, null);
+        String fileName=  BallColor.getImageOf("32x32");
+        //StatusImage statusImage = new StatusImage(fileName);
+        BallColor.values();
+        BallColor ball = new BallColor();
+        BallColor ball = mock(BallColor.class);
+        ImageTester.getImage(BallColor.ABORTED_ANIME,style,status,subject, colorName, null, null);
 
+    }
+
+    @Test
+    void testName() {
+        
     }
 
 
