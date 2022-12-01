f4fad8d7e8c7d22c8e8f8360541de949f5cd1c2c
Okhan-coder <O_khan4@u.pacific.edu>
2022-12-01 02:03:10 -0800

    Added More tests

--- src/test/java/org/jenkinsci/plugins/badge/ImageResolverTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/ImageResolverTest.java
@@ -30,14 +30,14 @@ public class ImageResolverTest {
     @Test
     public void TestGetDefault32x32Ball() throws Exception {
         ImageResolver ImageTester = new ImageResolver();
-        String style="ball-Error"; // should give default due to url being null
+        String style="ball-null"; // should give default due to url being null
         String status = null;
         String subject = null;
         String colorName=null;
         String fileName = "images/32x32/blue.png";
         BallColor ball = mock(BallColor.class);
         //default
-        Mockito.when(ball.getImageOf("Error")).thenReturn(null);
+        Mockito.when(ball.getImageOf("null")).thenReturn(null);
         Mockito.when(ball.getImageOf("32x32")).thenReturn(fileName);
         Mockito.when(ball.noAnime()).thenCallRealMethod();
         StatusImage TestImage = ImageTester.getImage(ball, style,subject,status, colorName, null, null);
@@ -45,6 +45,43 @@ public class ImageResolverTest {
 
      
     }
+    @Test
+    public void TestGetNonDefaultBall() throws Exception {
+        ImageResolver ImageTester = new ImageResolver();
+        String style="ball-16x16"; // should give url
+        String status = null;
+        String subject = null;
+        String colorName=null;
+        String fileName = "images/16x16/red.png";
+        BallColor ball = mock(BallColor.class);
+        //default
+        Mockito.when(ball.getImageOf("null")).thenReturn(null);
+        Mockito.when(ball.getImageOf("16x16")).thenReturn(fileName);
+        Mockito.when(ball.noAnime()).thenCallRealMethod();
+        StatusImage TestImage = ImageTester.getImage(ball, style,subject,status, colorName, null, null);
+        assertThat(TestImage.getEtag(), containsString(fileName));
+
+     
+    }
+    @Test
+    public void testShouldReturnEmpty() throws Exception {
+        ImageResolver ImageTester = new ImageResolver();
+        String style="ball-42x45"; // should throw exception
+        String status = null;
+        String subject = null;
+        String colorName=null;
+        String fileName = "DoesNotExist";;
+        BallColor ball = mock(BallColor.class);
+        //default
+        Mockito.when(ball.getImageOf("null")).thenReturn(null);
+        Mockito.when(ball.getImageOf("42x45")).thenReturn(fileName);
+        Mockito.when(ball.noAnime()).thenCallRealMethod();
+        StatusImage TestImage = ImageTester.getImage(ball, style,subject,status, colorName, null, null);
+        assertThat(TestImage.getEtag(), containsString("empty"));
+
+     
+    }
 }
+
 
 
