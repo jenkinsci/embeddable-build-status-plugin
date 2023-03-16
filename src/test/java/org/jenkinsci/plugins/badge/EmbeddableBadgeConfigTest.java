2ef22ee06a2397db687a48e367cd501c84919ce2
Wei Liao <wei.liao@disneystreaming.com>
2023-03-16 11:28:18 -0400

    added tests for EmbeddableBadgeConfig

---
+++ src/test/java/org/jenkinsci/plugins/badge/EmbeddableBadgeConfigTest.java
@@ -0,0 +1,44 @@
+package org.jenkinsci.plugins.badge;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.hamcrest.core.IsNull;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class EmbeddableBadgeConfigTest {
+    @Test
+    public void testConstructor() {
+        String id = "testId";
+        EmbeddableBadgeConfig embeddableBadgeConfig = new EmbeddableBadgeConfig(id);
+        assertThat(embeddableBadgeConfig.getID(), is(id));
+        assertThat(embeddableBadgeConfig.getSubject(), IsNull.nullValue());
+        assertThat(embeddableBadgeConfig.getStatus(), IsNull.nullValue());
+        assertThat(embeddableBadgeConfig.getAnimatedOverlayColor(), IsNull.nullValue());
+        assertThat(embeddableBadgeConfig.getLink(), IsNull.nullValue());
+        assertThat(embeddableBadgeConfig.getColor(), IsNull.nullValue());
+    }
+
+    @Test
+    public void testGetAnimatedOverlayColorBlueForRunning(){
+        EmbeddableBadgeConfig embeddableBadgeConfig = new EmbeddableBadgeConfig("testId");
+        embeddableBadgeConfig.setStatus("running");
+        assertThat(embeddableBadgeConfig.getAnimatedOverlayColor(), is("blue"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "failing,red", 
+        "passing,brightgreen", 
+        "unstable,yellow", 
+        "aborted,aborted", 
+        "running,blue"
+    })
+    public void testGetColor(String status, String expected){
+        EmbeddableBadgeConfig embeddableBadgeConfig = new EmbeddableBadgeConfig("testId");
+        embeddableBadgeConfig.setStatus(status);
+        assertThat(embeddableBadgeConfig.getColor(), is(expected));
+    }
+}
