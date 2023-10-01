f6a526107f4660cd3768cd3e07201d44d14c1572
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-01 23:54:00 +0530

    updated

---
+++ src/test/java/org/jenkinsci/plugins/badge/PluginImplTest.java
@@ -0,0 +1,15 @@
+package org.jenkinsci.plugins.badge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PluginImplTest {
+
+    @Test
+    void testStart() {
+        String expectation = "Success";
+        assertEquals(expectation, "Success");
+
+    }
+}
