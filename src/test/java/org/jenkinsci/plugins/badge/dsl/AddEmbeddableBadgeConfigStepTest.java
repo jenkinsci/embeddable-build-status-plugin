08b8ad889fe9fe6d67e4c0182eb1a49ba8c1dc35
Abhishek Maity <abhishek.maity@hotmail.com>
2023-10-02 19:58:37 +0530

    removed unused packages

--- src/test/java/org/jenkinsci/plugins/badge/dsl/AddEmbeddableBadgeConfigStepTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/dsl/AddEmbeddableBadgeConfigStepTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AddEmbeddableBadgeConfigStepTest {
     @Test
