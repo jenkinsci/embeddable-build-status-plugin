a64af16abfd744ef9ae76b63bb228144a4b69436
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2023-10-02 22:32:34 +0530

    [JENKINS-70464] Improve test coverage `AddEmbeddableBadgeConfigStepTest.java` (#244)
    * added 'AddEmbeddableBadgeConfigStepTest' for test coverage increase
    
    * removed unused packages
    
    * mvn spotless:apply applied

---
+++ src/test/java/org/jenkinsci/plugins/badge/dsl/AddEmbeddableBadgeConfigStepTest.java
@@ -0,0 +1,28 @@
+package org.jenkinsci.plugins.badge.dsl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+
+import org.junit.Test;
+
+public class AddEmbeddableBadgeConfigStepTest {
+    @Test
+    public void testConstructor() {
+        AddEmbeddableBadgeConfigStep addEmbeddableBadgeConfigStep =
+                new AddEmbeddableBadgeConfigStep("test-Id-constructor");
+        assertThat(addEmbeddableBadgeConfigStep.getID(), is("test-Id-constructor"));
+        assertThat(addEmbeddableBadgeConfigStep.getSubject(), is(nullValue()));
+        assertThat(addEmbeddableBadgeConfigStep.getStatus(), is(nullValue()));
+        assertThat(addEmbeddableBadgeConfigStep.getColor(), is(nullValue()));
+        assertThat(addEmbeddableBadgeConfigStep.getAnimatedOverlayColor(), is(nullValue()));
+        assertThat(addEmbeddableBadgeConfigStep.getLink(), is(nullValue()));
+    }
+
+    @Test
+    public void testGetAnimatedOverlayColor() {
+        AddEmbeddableBadgeConfigStep addEmbeddableBadgeConfigStep =
+                new AddEmbeddableBadgeConfigStep("test-animated-overlay-color");
+        assertThat(addEmbeddableBadgeConfigStep.getColor(), is(nullValue()));
+    }
+}
