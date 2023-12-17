2f018e882f8f6abf0b60ef79dae74c20fb50ca5b
Devesh chouhan <chouhanlim2003@gmail.com>
2023-12-17 13:48:22 +0000

    Increasing test coverage with adding setSubject(String), setLink(String) and setColor(String).

--- src/test/java/org/jenkinsci/plugins/badge/EmbeddableBadgeConfigTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/EmbeddableBadgeConfigTest.java
@@ -35,11 +35,66 @@ public class EmbeddableBadgeConfigTest {
         embeddableBadgeConfig.setStatus(status);
         assertThat(embeddableBadgeConfig.getColor(), is(expected));
     }
+
     @Test
-    public void testGetSubject() {
-    String subject = "Test Subject";
-    EmbeddableBadgeConfig embeddableBadgeConfig = new EmbeddableBadgeConfig("testId-subject");
-    embeddableBadgeConfig.setSubject(subject);
-    assertThat(embeddableBadgeConfig.getSubject(), is(subject));
-}
+    public void testSetSubject() {
+        String subject = "Test Subject";
+        EmbeddableBadgeConfig embeddableBadgeConfig = new EmbeddableBadgeConfig("testId-subject");
+        embeddableBadgeConfig.setSubject(subject);
+        assertThat(embeddableBadgeConfig.getSubject(), is(subject));
+    }
+
+    @Test
+    public void testSetValidLink() {
+        String link = "https://jenkins.io";
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-valid-link");
+        config.setLink(link);
+        assertThat(config.getLink(), is(link));
+    }
+
+    @Test
+    public void testSetEmptyLink() {
+        String link = "";
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-empty-link");
+        config.setLink(link);
+        assertThat(config.getLink(), is(link));
+    }
+
+    @Test
+    public void testSetValidDefaultColor() {
+        String color = "red";
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-valid-default-color");
+        config.setColor(color);
+        assertThat(config.getColor(), is(color));
+    }
+
+    @Test
+    public void testSetValidCustomColor() {
+        String color = "#ff00ff"; // Magenta
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-valid-custom-color");
+        config.setColor(color);
+        assertThat(config.getColor(), is(color));
+    }
+
+    @Test
+    public void testSetEmptyColor() {
+        String color = "";
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-empty-color");
+        config.setColor(color);
+        assertThat(config.getColor(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetNullColor() {
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-null-color");
+        config.setColor(null);
+        assertThat(config.getColor(), is(nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetInvalidColorName() {
+        String color = "invalid_color";
+        EmbeddableBadgeConfig config = new EmbeddableBadgeConfig("test-invalid-color-name");
+        config.setColor(color);
+    }
 }
