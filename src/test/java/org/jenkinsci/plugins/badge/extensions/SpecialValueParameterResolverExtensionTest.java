191a6d7c27925c40f1d00fdb80c4acf0ed87c76d
Yash Pal <yashpal2104@gmail.com>
2024-10-23 19:53:00 +0530

    Added new additional tests to the SpecialValueParameterResolverExtensionTest.java class in the embeddable status plugins
    line coverage is 92% and branch coverage increased to 50%

--- src/test/java/org/jenkinsci/plugins/badge/extensions/SpecialValueParameterResolverExtensionTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/extensions/SpecialValueParameterResolverExtensionTest.java
@@ -73,4 +73,28 @@ class SpecialValueParameterResolverExtensionTest {
                 extension.resolve(mockRun, "buildId buildNumber duration startTime displayName description");
         assertThat(actualParameter, is("1234 1234 23:35 23:35 display name run description"));
     }
+
+    @Test
+    void resolveEmptyStringParameter(){
+        String actualParameter = extension.resolve(mockRun, "");
+        assertThat(actualParameter, is(""));
+    }
+
+    @Test
+    void resolvewithNullParameter(){
+        String actualParameter = extension.resolve(mockRun, null);
+        assertThat(actualParameter, is((String)null));
+    }
+
+    @Test
+    void unknownParameterShouldNotBeResolved() {
+        String actualParameter = extension.resolve(mockRun, "unknown");
+        assertThat(actualParameter, is("unknown"));
+    }
+
+    @Test
+    void testResolveWithNoMatchingParameters() {
+        String actualParameter = extension.resolve(mockRun, "noMatchingParameters");
+        assertThat(actualParameter, is("noMatchingParameters"));
+    }
 }
