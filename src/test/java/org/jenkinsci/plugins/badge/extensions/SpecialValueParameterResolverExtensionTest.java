12a13f69050ad82feaa3699312322b53d7f96e7e
Yash Pal <yashpal2104@gmail.com>
2024-10-25 15:48:55 +0530

    improved the readability of the code

--- src/test/java/org/jenkinsci/plugins/badge/extensions/SpecialValueParameterResolverExtensionTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/extensions/SpecialValueParameterResolverExtensionTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-class SpecialValueParameterResolverExtensionTest{
+class SpecialValueParameterResolverExtensionTest {
 
     private SpecialValueParameterResolverExtension extension;
     private Job mockProject;
