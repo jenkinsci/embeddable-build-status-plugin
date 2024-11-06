6f7c5864f02d750dcf0354dd60f21429d41e6f66
Yash Pal <yashpal2104@gmail.com>
2024-11-06 22:58:44 +0530

    Fixed the path URL and instead of reading response with scanner tried Jenkins.Webclient

--- src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionTest.java
@@ -62,6 +62,7 @@ public class PublicBuildStatusActionTest {
         // Check job status icon is "not run" before job runs
         JenkinsRule.JSONWebResponse json = webClient.getJSON(jobStatusUrl);
         String result = json.getContentAsString();
+        System.out.println(result);
         assertThat(result, containsString("<svg "));
         assertThat(result, not(containsString(SUCCESS_MARKER)));
         assertThat(result, containsString(NOT_RUN_MARKER));
