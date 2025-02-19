bf30fe0fabf83c0e5d866342fc24b8ada056a25c
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-02-19 21:38:14 +0200

    test(PublicBuildStatusAction): add tests for getProject method.
    - test method implicitly through doIcon and doText
    - create a helper methods for setup objects and cleaning up

--- src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionTest.java
@@ -5,19 +5,37 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 
+import hudson.ExtensionList;
 import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.model.Job;
 import hudson.model.Run;
+import hudson.model.User;
+import hudson.security.ACL;
+import hudson.security.ACLContext;
+import hudson.security.AuthorizationStrategy;
 import hudson.tasks.BatchFile;
 import hudson.tasks.Shell;
 import java.io.File;
 import java.io.IOException;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.badge.extensionpoints.JobSelectorExtensionPoint;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.JenkinsRule.DummySecurityRealm;
+import org.jvnet.hudson.test.MockAuthorizationStrategy;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
+import org.kohsuke.stapler.Stapler;
 
 public class PublicBuildStatusActionTest {
 
@@ -213,6 +231,130 @@ public class PublicBuildStatusActionTest {
             JenkinsRule.JSONWebResponse json = webClient.getJSON(url);
             String result = json.getContentAsString();
             assertThat(result, containsString(PASSING_MARKER));
+        }
+    }
+
+    // Helpers method
+    private void setupSecurity(String username) {
+        DummySecurityRealm securityRealm = j.createDummySecurityRealm();
+        MockAuthorizationStrategy authStrategy = new MockAuthorizationStrategy()
+                .grant(Jenkins.READ, Item.READ)
+                .everywhere()
+                .to("user")
+                .grant(Jenkins.READ)
+                .everywhere()
+                .to("user")
+                .grant(Item.READ)
+                .everywhere()
+                .to("user")
+                .grant(PublicBuildStatusAction.VIEW_STATUS)
+                .everywhere()
+                .to("admin");
+
+        j.jenkins.setSecurityRealm(securityRealm);
+        j.jenkins.setAuthorizationStrategy(authStrategy);
+    }
+
+    private void cleanupSecurity(AuthorizationStrategy originalStrategy, User user) throws IOException {
+        j.jenkins.setAuthorizationStrategy(originalStrategy);
+        j.jenkins.setSecurityRealm(null);
+        if (user != null) {
+            user.delete();
+        }
+    }
+
+    @Test
+    public void testDoText_WithMultipleJobSelectors() throws Exception {
+        JobSelectorExtensionPoint nullSelector = new JobSelectorExtensionPoint() {
+            @Override
+            public Job<?, ?> select(String jobName) {
+                return null;
+            }
+        };
+
+        JobSelectorExtensionPoint validSelector = new JobSelectorExtensionPoint() {
+            @Override
+            public Job<?, ?> select(String jobName) {
+                return job;
+            }
+        };
+
+        ExtensionList<JobSelectorExtensionPoint> extensionsList = ExtensionList.lookup(JobSelectorExtensionPoint.class);
+        extensionsList.add(0, nullSelector);
+        extensionsList.add(1, validSelector);
+
+        try {
+            String result = new PublicBuildStatusAction().doText(null, null, job.getName(), null);
+            assertNotNull(result);
+            assertEquals("Not built", result);
+            assertNull(nullSelector.select(job.getName()));
+            assertEquals(job, validSelector.select(job.getName()));
+        } finally {
+            extensionsList.remove(nullSelector);
+            extensionsList.remove(validSelector);
+        }
+    }
+
+    @Test
+    public void testDoText_whenJobHasNoPermissions() throws Exception {
+        JobSelectorExtensionPoint jobSelector = new JobSelectorExtensionPoint() {
+            @Override
+            public Job<?, ?> select(String jobName) {
+                return job;
+            }
+        };
+        ExtensionList<JobSelectorExtensionPoint> extensionList = ExtensionList.lookup(JobSelectorExtensionPoint.class);
+        extensionList.add(0, jobSelector);
+
+        AuthorizationStrategy originalStrategy = j.jenkins.getAuthorizationStrategy();
+        setupSecurity("alice");
+        User alice = j.jenkins.getUser("alice");
+
+        try (ACLContext c = ACL.as(User.getById("alice", true))) {
+            assertThrows(HttpResponses.HttpResponseException.class, () -> new PublicBuildStatusAction()
+                    .doText(null, null, job.getName(), null));
+        } finally {
+            extensionList.remove(jobSelector);
+            cleanupSecurity(originalStrategy, alice);
+        }
+    }
+
+    @Test
+    public void testDoIcon_WhenJobHasNoPermissions() throws Exception {
+        JobSelectorExtensionPoint jobSelector = new JobSelectorExtensionPoint() {
+            @Override
+            public Job<?, ?> select(String jobName) {
+                return job;
+            }
+        };
+        ExtensionList<JobSelectorExtensionPoint> extensionList = ExtensionList.lookup(JobSelectorExtensionPoint.class);
+        extensionList.add(0, jobSelector);
+
+        AuthorizationStrategy originalStrategy = j.jenkins.getAuthorizationStrategy();
+        setupSecurity("alice");
+        User alice = j.jenkins.getUser("alice");
+
+        Run<?, ?> build = job.scheduleBuild2(0).get();
+        j.assertBuildStatusSuccess(build);
+
+        try (ACLContext c = ACL.as(User.getById("alice", true))) {
+            HttpResponse response = new PublicBuildStatusAction()
+                    .doIcon(
+                            Stapler.getCurrentRequest2(),
+                            Stapler.getCurrentResponse2(),
+                            job.getName(),
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null);
+            assertNotNull(response);
+        } finally {
+            extensionList.remove(jobSelector);
+            cleanupSecurity(originalStrategy, alice);
         }
     }
 }
