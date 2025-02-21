f16ead947287406925fe51ed47221aa0a32da0fd
Mark Waite <mark.earl.waite@gmail.com>
2025-02-20 18:38:37 -0700

    Test the getRun method
    Already have permissions logic, reuse for another test.  Increases
    coverage without a lot of extra effort.

--- src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionSecurityTest.java
+++ src/test/java/org/jenkinsci/plugins/badge/actions/PublicBuildStatusActionSecurityTest.java
@@ -1,14 +1,17 @@
 package org.jenkinsci.plugins.badge.actions;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThrows;
 
 import hudson.ExtensionList;
 import hudson.Functions;
 import hudson.model.FreeStyleProject;
 import hudson.model.Item;
+import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.User;
 import hudson.security.ACL;
@@ -18,6 +21,7 @@ import hudson.tasks.Shell;
 import java.io.IOException;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.badge.extensionpoints.JobSelectorExtensionPoint;
+import org.jenkinsci.plugins.badge.extensionpoints.RunSelectorExtensionPoint;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -87,6 +91,47 @@ public class PublicBuildStatusActionSecurityTest {
     }
 
     @Test
+    public void testGetRun_whenNotRunAndJobHasNoPermissions() throws Exception {
+        RunSelectorExtensionPoint runSelector = (Job myJob, String selector, Run reference) -> null;
+        ExtensionList.lookup(RunSelectorExtensionPoint.class).add(0, runSelector);
+
+        try (ACLContext c = ACL.as(User.getById("alice", true))) {
+            HttpResponses.HttpResponseException ex = assertThrows(
+                    HttpResponses.HttpResponseException.class, () -> PublicBuildStatusAction.getRun(job, "", true));
+            assertThat(ex, instanceOf(HttpResponses.notFound().getClass()));
+        }
+    }
+
+    @Test
+    public void testGetRun_whenJobHasNoPermissions() throws Exception {
+        Run<?, ?> build = job.scheduleBuild2(0).get();
+        j.assertBuildStatusSuccess(build);
+
+        RunSelectorExtensionPoint runSelector = (Job myJob, String selector, Run reference) -> build;
+        ExtensionList.lookup(RunSelectorExtensionPoint.class).add(0, runSelector);
+
+        try (ACLContext c = ACL.as(User.getById("alice", true))) {
+            HttpResponses.HttpResponseException ex = assertThrows(
+                    HttpResponses.HttpResponseException.class,
+                    () -> PublicBuildStatusAction.getRun(job, build.getId(), true));
+            assertThat(ex, instanceOf(HttpResponses.notFound().getClass()));
+        }
+    }
+
+    @Test
+    public void testGetRun_whenJobHasNoPermissionsReturnsNull() throws Exception {
+        Run<?, ?> build = job.scheduleBuild2(0).get();
+        j.assertBuildStatusSuccess(build);
+
+        RunSelectorExtensionPoint runSelector = (Job myJob, String selector, Run reference) -> build;
+        ExtensionList.lookup(RunSelectorExtensionPoint.class).add(0, runSelector);
+
+        try (ACLContext c = ACL.as(User.getById("alice", true))) {
+            assertThat(PublicBuildStatusAction.getRun(job, build.getId(), false), is(nullValue()));
+        }
+    }
+
+    @Test
     public void testDoIcon_WhenJobHasNoPermissions() throws Exception {
         JobSelectorExtensionPoint jobSelector = (String jobName) -> job;
         ExtensionList.lookup(JobSelectorExtensionPoint.class).add(0, jobSelector);
@@ -97,7 +142,7 @@ public class PublicBuildStatusActionSecurityTest {
         try (ACLContext c = ACL.as(User.getById("alice", true))) {
             HttpResponse response = new PublicBuildStatusAction()
                     .doIcon(null, null, job.getName(), null, null, null, null, null, null, null, null);
-            assertNotNull(response);
+            assertThat(response.toString(), containsString("StatusImage"));
         }
     }
 }
